56aad18ae450abbe8149d07a89f6a3245e5ad79f
radiogron <kisasik1@gmail.com>
2019-04-15 02:49:15 +0400

    initial ov 2.0 version

--- gulpfile.js
+++ gulpfile.js
@@ -2,103 +2,73 @@
 
 //dependencies for build
 const gulp = require('gulp');
-const sass = require('gulp-sass');
-const sass_globbing = require('gulp-sass-glob');
+const sassToCss = require('gulp-sass');
+const sassGlobbing = require('gulp-sass-glob');
 const pug = require('gulp-pug');
-const plumber = require('gulp-plumber');
-const imagemin = require('gulp-imagemin');
-const flatten = require('gulp-flatten');
-const concat = require('gulp-concat');
-
-const argv = require('yargs').argv;
-const gulpif = require('gulp-if');
-const gulpifelse = require('gulp-if-else');
-const livereload = require('gulp-livereload');
-const connect = require('gulp-connect');
-const rename = require('gulp-rename');
+const pugPlumber = require('gulp-plumber');
+const pugConcat = require('gulp-concat');
 //dependencies for build END
 
-//settings
-var target = '';
-if( argv.project === 'player' ){
-	target = 'dist/player/'
-} else if ( argv.project === 'artist' ){
-	target = 'dist/artist/'
+//config INIT
+var buildData = require('./src/build.json');
+//config END
+
+
+function style(cb) {
+	buildData = require('./src/build.json');
+	for (let build of buildData.builds) {
+		console.log('style for: ' + build.title);
+		gulp
+			.src( build.src.styles )
+			.pipe( pugPlumber() )
+			.pipe( sassGlobbing() )
+			.pipe( pugConcat('style.sass'))
+			.pipe( sassToCss().on( 'error', sassToCss.logError ) )
+			.pipe( gulp.dest( build.dist ) );
+	}
+	return cb();
 }
-var source = {
-	style  : 'source/_assets/style/',
-	script : 'source/_assets/js/',
-	images : 'source/_assets/images/' + argv.project + '/',
-	pug	   : 'source/' + argv.project + '/',
+
+function markup(cb) {
+	buildData = require('./src/build.json');
+	for (let build of buildData.builds) {
+		console.log('markup for: ' + build.title);
+		let data = require(build.src.data);
+		console.log('data: ' + data);
+		gulp
+			.src( build.src.markup )
+			.pipe( pugPlumber() )
+			.pipe(pug({
+				locals: data,
+				pretty: true
+			}))
+			.pipe( gulp.dest( build.dist ) );
+	}
+	return cb();
 }
-var dist = {
-	style  : target + 'assets/css/',
-	script : target + 'assets/js/',
-	images : target + 'assets/images/',
-	fonts  : target + 'assets/fonts/',
-	html   : target
+
+function images(cb) {
+//	gulp
+//		.src(test+'pug/**/*.pug')
+//		.pipe( plumber() )
+//		.pipe( pug( { pretty: true } ) )
+//		.pipe( gulp.dest(  ) );
+	return cb();
 }
-gulp.task('sass', function () {
-	gulp.src( source.style + argv.project + '.sass' )
-		.pipe( sass() )
-		.pipe( sass_globbing() )
-		.pipe( rename({
-			basename: 'style',
-			extname: '.css'
-		}) )
-		.pipe( gulp.dest(dist.style) )
-		//.pipe( sass({ outputStyle: 'compressed' }) )
-		//.pipe( sass_globbing() )
-		//.pipe( rename({
-		//	basename: 'style.min',
-		//	extname: '.css'
-		//}) )
-		//.pipe( gulp.dest(dist.style) )
-});
-gulp.task('imagemin', function () {
-	gulp.src( source.images+'*' )
-		.pipe( imagemin([
-			imagemin.gifsicle( { interlaced: true } ),
-			imagemin.jpegtran( { progressive: true } ),
-			imagemin.optipng( { optimizationLevel: 5 } ),
-			imagemin.svgo( { plugins: [{removeViewBox: true}] } )
-		], {verbose: true}))
-		.pipe(flatten())
-		.pipe( gulp.dest( dist.images ) )
-});
-gulp.task('pug', function buildHTML() {
-	gulp.src(source.pug + '*.pug')
-		.pipe( plumber() )
-		.pipe( gulpifelse( !argv.build, function(){ return pug() }, function(){ return pug({ pretty: true })} ) )
-		.pipe( gulp.dest( dist.html ) )
-});
-gulp.task('scripts', function() {
-	return gulp.src(source.script + '*.js')
-		.pipe(concat('app.js'))
-		.pipe(gulp.dest(dist.script))
-});
-gulp.task('rage', function(){
-	// copy fonts
-	gulp.src('source/_modules/icons-rage/dist/fonts/*')
-		.pipe(flatten())
-		.pipe( gulp.dest( dist.fonts ) )
-	// copy css
-	gulp.src('source/_modules/icons-rage/dist/css/*.css')
-		.pipe(flatten())
-		.pipe( gulp.dest( dist.style ) )
-})
 //build project END
-gulp.task('default', ['watch','connect']);
-gulp.task('connect', function() {
-	connect.server({
-		livereload: true
-	});
-});
-//watchers
-gulp.task('watch', function(){
-	gulp.watch( source.style+'**/*.sass', ['sass'] );
-	gulp.watch( source.images+'*', ['imagemin'] );
-	gulp.watch( 'source/**/*.pug', ['pug'] );
-	gulp.watch( 'source/**/*.js', ['scripts'] );
-})
-//watchers END
+
+function watch() {
+	gulp.watch(['src/**/*.sass', 'src/**/*.json'], style);
+	gulp.watch(['src/**/*.pug', 'src/**/*.json'], markup);
+}
+
+exports.style = style
+exports.markup = markup
+exports.images = images
+exports.watch = watch
+
+const build = gulp.series(gulp.parallel(images, style, markup));
+const workon = gulp.series(build, watch);
+
+exports.build = build
+exports.default = workon
